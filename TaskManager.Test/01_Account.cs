08dde7d375bc0de206e1eef2f4b9e7626dd7f2ed
Anton <myautocall@gmail.com>
2025-02-09 19:31:19 +0700

    xTest

---
+++ TaskManager.Test/01_Account.cs
@@ -0,0 +1,63 @@
+using System.Net;
+using System.Text;
+using TaskManager.Common.Extensions;
+using TaskManager.Server.Models;
+using Xunit.Abstractions;
+
+namespace TaskManager.Test;
+
+public class Account_UnitTest {
+    private readonly ITestOutputHelper Output;
+    public Account_UnitTest(ITestOutputHelper output) {
+        Output = output;
+    }
+
+    [Fact]
+    public async Task Test01_SignUp() {
+        var cookieContainer = new CookieContainer();
+        var httpClient = new HttpClient(
+            new HttpClientHandler() {
+                CookieContainer = cookieContainer
+            }
+        );
+
+        var response = await httpClient.PostAsync($"{Settings.Site}/Api/Account/SignUp",
+            new StringContent(
+                JsonExtension.Serialize(new SignUpViewModel() {
+                    UserName = Settings.TestUserName,
+                    Email = Settings.TestUserEmail,
+                    Password = Settings.DefaultPassword }),
+                Encoding.UTF8, "application/json")
+        );
+        var data = await response.Content.ReadAsStringAsync();
+
+        Output.WriteLine($"StatusCode: {response.StatusCode}");
+        Output.WriteLine($"Response: {data}");
+        Assert.True(response.IsSuccessStatusCode);
+    }
+
+    [Fact]
+    public async Task Test02_SignIn() {
+        var cookieContainer = new CookieContainer();
+        var httpClient = new HttpClient(
+            new HttpClientHandler() {
+                CookieContainer = cookieContainer
+            }
+        );
+
+        var response = await httpClient.PostAsync($"{Settings.Site}/Api/Account/SignIn",
+            new StringContent(
+                JsonExtension.Serialize(new LoginViewModel() {
+                    Email = Settings.TestUserEmail + "!",
+                    Password = Settings.DefaultPassword
+                }),
+                Encoding.UTF8, "application/json")
+        );
+        var data = await response.Content.ReadAsStringAsync();
+
+        Output.WriteLine($"StatusCode: {response.StatusCode}");
+        Output.WriteLine($"Response: {data}");
+        Assert.True(response.IsSuccessStatusCode);
+    }
+
+}

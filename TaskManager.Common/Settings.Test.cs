ed208f513fce0cb8e368a46a77526c80acc7ebac
Anton <myautocall@gmail.com>
2025-02-11 20:52:32 +0700

    Changed the test settings (2)

--- TaskManager.Common/Settings.Test.cs
+++ TaskManager.Common/Settings.Test.cs
@@ -1,7 +1,7 @@
 ﻿namespace TaskManager;
 public partial interface Settings {
 #if TEST
-    public const string Site = "https://localhost:5173";
+    public const string Site = "https://localhost:7090";
     public const string PrivateKey = "bAafd@A7d9#@F4*V!LHZs#ebKQrkE6pad2f3kj34c3dXy@";
     public const string DefaultPassword = "123456";
     public const string MasterPassword = "tm123456";

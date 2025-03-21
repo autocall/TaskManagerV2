ac470d3bb1ea4f902809028a1e7cf1bf0093cd2c
Anton <myautocall@gmail.com>
2025-03-21 17:17:58 +0700

    Fixed Comment encoding

---
+++ TaskManager.Data/Migrations/20250321101709_AlterTable_Comment_Collate_UTF8.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTable_Comment_Collate_UTF8 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("alter table Comment alter column Text varchar(max) collate Latin1_General_100_CI_AI_SC_UTF8");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
